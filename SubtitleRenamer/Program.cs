bbcda5329834fcd270b320df582473a25a1454d8
Иван <prokvanya@yandex.ru>
2020-09-27 04:36:37 +0300

    теперь точно работает

--- SubtitleRenamer/Program.cs
+++ SubtitleRenamer/Program.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace SubtitleRenamer
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //TODO: параметры запуска, ручной ввод параметров
         {
+            string extVid = ".mkv";
+            string extSub = ".ass";
+            string dir = @"\";
+
+            string[] videos = Directory.GetFiles(dir, "*" + extVid);
+            string[] subtitles = Directory.GetFiles(dir, "*" + extSub);
+
+            for (int i = 0; i < Math.Min(videos.Length, subtitles.Length); i++)
+            {
+                File.Move(videos[i], subtitles[i].Replace(extSub, extVid));
+            }
         }
     }
 }
